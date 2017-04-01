69e723d4c0e4d4c1a0b97e5e2e483a35f1b6e785
dktobeno1 <zeqizai@163.com>
2017-04-01 17:02:57 +0800

    Update and rename selenium.py to selenium_completed.py

---
+++ selenium_completed.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+import re,time
+from bs4 import BeautifulSoup
+import json
+
+def get_gd(name_company):
+    my_driver = webdriver.PhantomJS()
+    url_first = r'http://www.tianyancha.com/search?key=%s&checkFrom=searchBox'%name_company
+    my_driver.get(url_first)
+    time.sleep(15)
+    content_first = my_driver.page_source
+    pattern = re.compile(r'href="http://www.tianyancha.com/company/(.+?)"')
+    list_id = re.findall(pattern,content_first)
+    url_gd = r'http://www.tianyancha.com/expanse/changeinfo.json?id=%s&ps=5&pn=2'%list_id[0]
+    my_driver.get(url_gd)
+    time.sleep(5)
+    content_gd = my_driver.page_source
+    my_driver.close()
+    soup = BeautifulSoup(content_gd,'lxml')
+    pre = soup.pre.string
+    dic_gd = json.loads(pre)['data']['result'][1]
+    print(dic_gd)
+
+
+if __name__ == '__main__':
+    name = input('请输入需要查询的公司名：')
+    name = str(name.encode('utf-8')).replace(r'\x',r'%').upper()
+    name = name.split(r"'")[1]
+    #print(name)
+    get_gd(name)
