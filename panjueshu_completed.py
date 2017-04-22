5c30123bf731668226791b912b2d5fa200ca4448
dktobeno1 <zeqizai@163.com>
2017-04-22 20:12:51 +0800

    Update panjueshu_completed.py

--- panjueshu_completed.py
+++ panjueshu_completed.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import re,requests,time,random,queue
+import re,requests,time,random,queue,json
 import multiprocessing
 import mysql.connector
+
+anyou = '破坏社会主义市场经济秩序'
+year = '2016'
 
 def randHeader():  # 随机获取headers头
     head_user_agent = ['Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
@@ -46,54 +49,36 @@ def randHeader():  # 随机获取headers头
 def get_ips():
     list_ip = ['125.88.74.122:81',
                '183.95.80.165:8080',
-               '59.37.160.57:8081',
                '124.133.230.254:80',
                '183.78.183.156:82',
                '124.88.67.83:843',
                '42.81.58.199:80',
-               '124.88.67.9:80',
-               '180.167.34.187:80',
                '183.78.183.156:82',
                '125.88.74.122:85',
-               '124.88.67.22:83',
-               '14.152.93.79:8080',
                '182.38.36.42:8998',
                '122.96.59.98:82',
-               '117.95.19.112:8998',
-               '14.127.200.59:8081',
                '221.3.6.2:8081',
-               '182.112.128.115:80',
-               '124.88.67.9:843',
                '171.8.79.143:8080',
-               '180.136.83.16:8998',
                '220.174.236.211:80',
-               '124.88.67.17:83',
                '42.81.58.199:80',
                '183.165.150.216:8998',
                '183.78.183.156:82',
-               '218.26.227.108:80',
-               '124.88.67.9:83',
                '175.30.124.128:80',
-               '180.169.59.222:8080',
                '60.250.72.252:8080',
                '222.92.141.250:80',
                '202.107.222.50:80',
                '122.116.229.240:80',
                '112.11.126.198:80',
-               '124.88.67.34:82',
                '220.174.236.211:80',
                '119.254.84.90:80',
                '118.122.250.109:80',
                '211.140.151.220:80',
                '118.99.178.21:8080',
                '222.169.193.162:8099',
-               '111.207.231.14:8080',
                '112.11.126.202:80',
-               '180.167.34.187:80',
                '120.132.6.206:80',
                '122.228.179.178:80',
                '218.4.101.130:83',
-               '122.193.14.102:80',
                '111.12.96.188:80',
                '111.7.174.135:80',
                '101.204.163.82:8080',
@@ -124,10 +109,104 @@ def get_ips():
                '112.11.126.201:8080',
                '42.81.58.199:80',
                '118.99.178.21:8080',
-               '14.152.93.79:8080',
                '61.191.41.130:80',
                '42.81.58.198:80',
-               '222.73.27.178:80'
+               '222.73.27.178:80',
+               '61.135.217.3:80',
+               '61.191.41.130:80',
+               '222.92.141.250:80',
+               '218.4.101.130:83',
+               '111.206.163.235:80',
+               '125.140.111.61:80',
+               '223.100.12.12:80',
+               '124.88.67.63:80',
+               '119.254.84.90:80',
+               '124.133.230.254:80',
+               '122.228.179.178:80',
+               '111.13.7.42:83',
+               '222.73.27.178:80',
+               '222.76.217.25:80',
+               '61.139.104.216:80',
+               '211.140.151.220:80',
+               '58.23.16.240:80',
+               '124.88.67.13:81',
+               '222.92.141.250:80',
+               '61.139.104.216:80',
+               '124.88.67.31:81',
+               '118.122.250.109:80',
+               '122.228.179.178:80',
+               '218.4.101.130:83',
+               '222.76.217.25:80',
+               '111.13.7.42:81',
+               '124.88.67.63:80',
+               '111.206.163.235:80',
+               '61.135.217.3:80',
+               '211.140.151.220:80',
+               '124.88.67.32:82',
+               '119.254.84.90:80',
+               '183.78.183.156:82',
+               '58.23.16.240:80',
+               '124.235.182.130:80',
+               '122.142.77.84:80',
+               '180.167.34.187:80',
+               '111.13.7.42:81',
+               '218.26.227.108:80',
+               '124.88.67.35:82',
+               '61.135.217.3:80',
+               '218.4.101.130:83',
+               '118.122.250.109:80',
+               '183.254.149.120:80',
+               '122.228.179.178:80',
+               '124.88.67.54:81',
+               '124.88.67.20:82',
+               '124.88.67.30:83',
+               '124.88.67.52:81',
+               '111.206.163.235:80',
+               '124.133.230.254:80',
+               '111.12.96.188:80',
+               '61.191.41.130:80',
+               '124.88.67.31:83',
+               '124.202.198.182:80',
+               '124.88.67.54:80',
+               '58.23.16.240:80',
+               '122.228.179.178:80',
+               '223.100.12.12:80',
+               '124.88.67.35:83',
+               '218.4.101.130:83',
+               '118.122.250.109:80',
+               '119.254.84.90:80',
+               '124.133.230.254:80',
+               '124.88.67.20:83',
+               '61.191.41.130:80',
+               '124.88.67.19:81',
+               '222.76.217.25:80',
+               '222.92.141.250:80',
+               '218.241.153.211:81',
+               '61.135.217.3:80',
+               '61.139.104.216:80',
+               '211.140.151.220:80',
+               '111.13.7.42:83',
+               '175.173.206.232:80',
+               '183.190.148.221:80',
+               '124.88.67.13:81',
+               '111.206.163.235:80',
+               '124.88.67.22:80',
+               '222.169.193.162:8099',
+               '61.135.217.3:80',
+               '122.228.179.178:80',
+               '119.254.84.90:80',
+               '124.88.67.32:80',
+               '218.27.165.22:80',
+               '183.190.148.221:80',
+               '118.122.250.109:80',
+               '218.241.153.211:81',
+               '222.169.193.162:8099',
+               '111.13.7.42:81',
+               '222.76.217.25:80',
+               '111.206.163.235:80',
+               '111.12.96.188:80',
+               '222.92.141.250:80',
+               '223.100.12.12:80'
                ]
     return list_ip
 
@@ -146,7 +225,7 @@ def failHandler():
                 doc_url = 'http://wenshu.court.gov.cn/CreateContentJS/CreateContentJS.aspx?DocID=%s'%docid
                 id = t[0]
                 for a in range(5):
-                    time.sleep(a*2)
+                    time.sleep(a*1.5)
                     try:
                         #headers = randHeader()
                         time.sleep(random.randint(0,2)+random.random())
@@ -190,9 +269,12 @@ def failHandler():
 #            continue
 #    ip_list = obtan.text.split('\r\n')
 #    return ip_list
+
 def worker(que):
-    qishi = (que.get()-1)*20+1
-    jieshu = qishi+19
+    province = que.get()
+    print(province)
+    #qishi = 1
+    jieshu = 100#不是定值
     proxy_ip = {}
     ip_time = 0
     ip_list=[]
@@ -202,7 +284,7 @@ def worker(que):
     conn = mysql.connector.connect(user="root", password="", database="caipan")
     cursor = conn.cursor()
     the_url = 'http://wenshu.court.gov.cn/List/ListContent'
-    index = qishi-1
+    index = 0
     try_j = 0
     while True:
         if not s.empty():
@@ -222,11 +304,11 @@ def worker(que):
             index += 1
             try_j = 0
             if index > jieshu:
-                print('>>>>>>>>>>>>> %s -- %s 页文书已抓取完成  <<<<<<<<<<<<<<'% (qishi,jieshu))
+                print('>>>>>>>>>>>>> %s 年 %s  %s 判决书抓取完毕<<<<<<<<<<<<<<'% (year,province,anyou))
                 break
                 
         try:
-            data={'Param':'裁判年份:2016',
+            data={'Param':'裁判年份:%s,二级案由:%s,法院地域:%s'% (year,anyou,province),
             'Page':'20',
             'Order':'法院层级',
             'Index':str(index),
@@ -234,7 +316,7 @@ def worker(que):
 
             }
             headers = randHeader()
-            time.sleep(random.randint(0,1) + random.random())
+            time.sleep(random.random())
             #if (int(time.time())-ip_list_time) >500 or not ip_list:
             #    ip_list = getips()
             #    print(ip_list)
@@ -243,30 +325,36 @@ def worker(que):
             if (int(time.time())-ip_time) > 60 or not proxy_ip:
                 proxy_ip={'http':'http://%s'%random.choice(get_ips())}
                 ip_time=int(time.time())
-            print('正在爬取第%s页'%index)
+            print('正在爬取%s第%s页'%(province,index))
             resp=requests.post(the_url,data = data,headers = headers[0],proxies=proxy_ip,timeout=40)
             time.sleep(1)
-            respons=resp.text
-            pattern=re.compile(r'{\\"裁判要旨段原文\\":\\"(.*?)\\",\\"案件类型\\":\\"(.*?)\\",\\"裁判日期\\":\\"(.*?)\\",\\"案件名称\\":\\"(.*?)\\",\\"文书ID\\":\\"(.*?)\\",\\"审判程序\\":\\"(.*?)\\",\\"案号\\":\\"(.*?)\\",\\"法院名称\\":\\"(.*?)\\"}')
-            list_info = re.findall(pattern,respons)
-            print('第%s页有%s条'%(index,len(list_info)))
-            if len(list_info) == 0:
-                print(respons,proxy_ip,'被禁')
-                s.put(index)
-                proxy_ip={'http':'http://%s'%random.choice(get_ips())}
-                ip_time=int(time.time())
-                print ("(Post)未访问成功，压入队列 %s (IP被禁)"%index)
-                continue
-            print('第%s页概要爬取成功'%index)
-            #try_j = 0
+            #respons=resp.text
+            #pattern=re.compile(r'{\\"裁判要旨段原文\\":\\"(.*?)\\",\\"案件类型\\":\\"(.*?)\\",\\"裁判日期\\":\\"(.*?)\\",\\"案件名称\\":\\"(.*?)\\",\\"文书ID\\":\\"(.*?)\\",\\"审判程序\\":\\"(.*?)\\",\\"案号\\":\\"(.*?)\\",\\"法院名称\\":\\"(.*?)\\"}')
+            #list_info = re.findall(pattern,respons)
+            list_info = json.loads(json.loads(resp.text))
+            print('第%s页有%s条(%s)'%(index,len(list_info)-1,province))
+            #if len(list_info) == 0:
+            #    print(respons,proxy_ip,'被禁')
+            #    s.put(index)
+            #    proxy_ip={'http':'http://%s'%random.choice(get_ips())}
+            #    ip_time=int(time.time())
+            #    print ("(Post)未访问成功，压入队列 %s (%s)"%(index,province))
+            #    continue
+            print('第%s页概要爬取成功(%s)'%(index,province))
         except:
             print(proxy_ip,'request出错')
             s.put(index)
             proxy_ip={'http':'http://%s'%random.choice(get_ips())}
             ip_time=int(time.time())
-            print ("(Post)未访问成功，压入队列 %s (request出错)"%index)
+            print ("(Post)未访问成功，压入队列 %s (%s)"%(index,province))
             continue
-
+        if index == 1:
+            count = int(list_info[0]['Count'])
+            if count%20 == 0:
+                jieshu = count//20
+            else:
+                jieshu = count//20 + 1
+            print('%s共%s页'% (province,jieshu))
 
         i = 0 #一页中的第i个案例
         try_i = 0 #一个案例的重试次数
@@ -275,30 +363,30 @@ def worker(que):
                 i = q.get()
                 try_i += 1
                 time.sleep(try_i * 1.5)
-                if try_i > 5:
+                if try_i > 4:
                     cursor.execute('insert into failure (name, yiju, docid, produce, type, num, court, date, url, doc_url) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',[name, yiju, docid, produce, type, num, court, date, url, doc_url])
                     conn.commit()
-                    print ('!!!!!!!!!!!!!!!!!!! 请求过多,%s页第%s个案件:%s 写入Failure表中 !!!!!!!!!!!!!!!!!!!'% (index, i, name))
+                    print ('!!!!!!!!!!!!!!!!!!! 请求过多,%s页第%s个案件:%s 写入Failure表中(%s) !!!!!!!!!!!!!!!!!!!'% (index, i, name, province))
                     i += 1
                     try_i = 0
                     q.queue.clear()
-                    if i > len(list_info):
-                        print('第%s页爬取完成'%index)
+                    if i > len(list_info)-1:
+                        print('第%s页爬取完成(%s)'%(index, province))
                         break
             else:
                 i += 1
                 try_i = 0
-                if i > len(list_info):
-                    print('第%s页爬取完成'%index)
+                if i > len(list_info)-1:
+                    print('第%s页爬取完成(%s)'%(index, province))
                     break
-            yiju = list_info[i-1][0]
-            type = list_info[i-1][1]
-            date = list_info[i-1][2]
-            name = list_info[i-1][3]
-            docid = list_info[i-1][4]
-            produce = list_info[i-1][5]
-            num = list_info[i-1][6]
-            court = list_info[i-1][7]
+            yiju = list_info[i]['裁判要旨段原文']
+            type = list_info[i]['案件类型']
+            date = list_info[i]['裁判日期']
+            name = list_info[i]['案件名称']
+            docid = list_info[i]['文书ID']
+            produce = list_info[i]['审判程序']
+            num = list_info[i]['案号']
+            court = list_info[i]['法院名称']  
             url='http://wenshu.court.gov.cn/content/content?DocID=%s'%docid
             doc_url = 'http://wenshu.court.gov.cn/CreateContentJS/CreateContentJS.aspx?DocID=%s'%docid
             try:
@@ -307,17 +395,17 @@ def worker(que):
                     proxy_ip={'http':'http://%s'%random.choice(get_ips())}
                     ip_time=int(time.time())
                 html = requests.get(doc_url, proxies=proxy_ip, timeout=5)
-                print(html.text)
+                #print(html.text)
                 pattern = re.compile(r'jsonHtmlData = "(.*?)";')
                 content = re.findall(pattern,html.text)[0]
             except:
                 q.put(i)
                 proxy_ip={'http':'http://%s'%random.choice(get_ips())}
                 ip_time=int(time.time())
-                print ("(Get) %s未抓取成功,压入队列 "%name) 
+                print ("(Get) %s未抓取成功,压入队列(%s) "% (name, province)) 
                 print(doc_url)
                 continue
-            print('第%s页第%s个案件导入数据库'% (index, i))
+            print('第%s页第%s个案件导入数据库(%s)'% (index, i, province))
             sql = 'insert into panjueshu (name, yiju, docid, produce, type, num, court, date, content, url) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'
             cursor.execute(sql,[name, yiju, docid, produce, type, num, court, date, content, url])
         conn.commit()               
@@ -327,10 +415,13 @@ def worker(que):
     conn.close()
 if __name__=='__main__':
     q = multiprocessing.Queue()
-    for a in range(1,6):
+    list_province = ['北京市', '广东省', '浙江省', '江苏省', '河南省', '四川省', '河北省', '山西省', '山东省', '湖北省', '湖南省', '辽宁省', '吉林省', '福建省', '安徽省',
+               '黑龙江省', '上海市', '江西省', '内蒙古自治区', '广西壮族自治区', '海南省', '重庆市', '贵州省', '天津市', '云南省', '西藏自治区', '陕西省', '甘肃省',
+               '青海省', '宁夏回族自治区', '新疆维吾尔自治区', '新疆维吾尔自治区高级人民法院生产建设兵团分院']
+    for a in list_province:
         q.put(a)
     r = []
-    for b in range(3):
+    for b in range(6):
         print('进程 %s'%(b+1))
         p = multiprocessing.Process(target = worker,args = (q,))
         p.start()
